221ba2483c472c6e08acc62f4f7572c909604ad9
Anton Alekseev <anton@thorgate.eu>
2020-12-26 18:20:20 +0200

    Add service worker

--- src/index.js
+++ src/index.js
@@ -18,60 +18,22 @@ const DEFAULT_DURATIONS = {
   [LONG_BREAK]: 15,
 };
 
-const app = () => ({
-  actions: [POMODORO, SHORT_BREAK, LONG_BREAK],
-  colors: [RED, CYAN, VIOLET],
-  fonts: [KUMBH_SANS, ROBOTO_SERIF, SPACE_MONO],
+const INITIAL_SETTINGS = {
+  open: false,
+  font: KUMBH_SANS,
+  color: RED,
+  durations: { ...DEFAULT_DURATIONS },
+  _raw: { durations: { ...DEFAULT_DURATIONS }, color: RED, font: KUMBH_SANS },
+  _errors: {
+    [POMODORO]: false,
+    [SHORT_BREAK]: false,
+    [LONG_BREAK]: false,
+  },
+};
 
-  activeAction: POMODORO,
-  runningTimer: null,
-  duration: minutesToSeconds(DEFAULT_DURATIONS[POMODORO]),
+const timestampInSeconds = () => Math.floor(Date.now() / 1000);
 
-  start() {
-    this.runningTimer = setInterval(() => {
-      if (this.duration <= 0) {
-        this.resetTimers();
-        return;
-      }
-      this.duration -= 1;
-    }, 1000);
-  },
-  pause() {
-    clearInterval(this.runningTimer);
-    this.runningTimer = null;
-  },
-  resetTimers() {
-    clearInterval(this.runningTimer);
-    this.runningTimer = null;
-    this.duration = minutesToSeconds(this.settings.durations[this.activeAction]);
-  },
-  setAction(action) {
-    this.activeAction = action;
-    this.resetTimers();
-  },
-
-  settings: {
-    open: false,
-    font: KUMBH_SANS,
-    color: RED,
-    durations: { ...DEFAULT_DURATIONS },
-    _raw: { durations: { ...DEFAULT_DURATIONS }, color: RED, font: KUMBH_SANS },
-    _errors: {
-      [POMODORO]: false,
-      [SHORT_BREAK]: false,
-      [LONG_BREAK]: false,
-    },
-  },
-
-  validate(value, action) {
-    if (value > 59 || value < 1 || value % 1 !== 0) {
-      this.settings._errors[action] = true;
-    } else {
-      this.settings._errors[action] = false;
-      this.settings._raw.durations[action] = value;
-    }
-  },
-});
+const minutesToSeconds = (minutes) => minutes * 60;
 
 // Shamelessly copied from here
 // https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript#19700358
@@ -86,10 +48,6 @@ const secondsToMinutes = (duration) => {
   return minutes + ":" + seconds;
 };
 
-const minutesToSeconds = (minutes) => {
-  return minutes * 60;
-};
-
 // I hate calculating percentages so much
 const durationToPercents = (currentDuration, fullDuration) => {
   const fullDurationSeconds = minutesToSeconds(fullDuration);
@@ -98,3 +56,77 @@ const durationToPercents = (currentDuration, fullDuration) => {
   // Subtracting from 100% to get reversed percentage
   return 100 - durationDifference / singlePercent;
 };
+
+const app = () => ({
+  actions: [POMODORO, SHORT_BREAK, LONG_BREAK],
+  colors: [RED, CYAN, VIOLET],
+  fonts: [KUMBH_SANS, ROBOTO_SERIF, SPACE_MONO],
+
+  offline: false,
+
+  activeAction: POMODORO,
+  runningTimer: null,
+  timerDuration: null,
+  settings: INITIAL_SETTINGS,
+
+  init() {
+    // Checking IndexDB for pre-populated values
+    ["activeAction", "timerDuration", "settings"].forEach((key) => {
+      idbKeyval.get(key).then((value) => {
+        if (value) {
+          this[key] = value;
+        }
+      });
+    });
+    if (!this.timerDuration) {
+      this.timerDuration = minutesToSeconds(this.settings.durations[this.activeAction]);
+    }
+    // This will trigger recalculating of the page heigh
+    // after Alpine finished component initialization
+    window.dispatchEvent(new Event("resize"));
+  },
+
+  start() {
+    this.runningTimer = setInterval(() => {
+      if (this.timerDuration <= 0) {
+        this.resetTimers();
+        return;
+      }
+      this.timerDuration -= 1;
+      idbKeyval.set("timerDuration", this.timerDuration);
+    }, 1000);
+  },
+  pause() {
+    clearInterval(this.runningTimer);
+    this.runningTimer = null;
+  },
+  resetTimers() {
+    this.pause();
+    idbKeyval.del("timerDuration");
+    this.timerDuration = minutesToSeconds(this.settings.durations[this.activeAction]);
+  },
+  setAction(action) {
+    this.activeAction = action;
+    idbKeyval.set("activeAction", action);
+    this.resetTimers();
+  },
+  validate(value, action) {
+    if (value > 59 || value < 1 || value % 1 !== 0) {
+      this.settings._errors[action] = true;
+    } else {
+      this.settings._errors[action] = false;
+      this.settings._raw.durations[action] = value;
+    }
+  },
+  applySettings() {
+    // Deep copy settings object
+    const newSettings = JSON.parse(JSON.stringify({ ...this.settings, ...this.settings._raw }));
+    this.settings = newSettings;
+    idbKeyval.set("settings", newSettings);
+  },
+});
+
+if ("serviceWorker" in navigator) {
+  // register service worker
+  navigator.serviceWorker.register("/service-worker.js", { scope: "./" });
+}
